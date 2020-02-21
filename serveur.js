7424d2e75947e9623b72567a124c2ac0bf86e15b
ElodieWDJ <persepolis34@live.fr>
2020-02-21 09:12:04 +0100

    token

--- serveur.js
+++ serveur.js
@@ -8,17 +8,22 @@ let server = http.createServer(function (request, response) {
  
     if(request.method== "GET"){
         response.setHeader('Access-Control-Allow-Origin', '*');
-        if (queryObject.user == 'jean') {
+        console.log("get");
+        if (queryObject.user == 'jean.jean@jean.com' && queryObject.password == 'jean') {
+            console.log("---- 200")
             response.writeHead(200, { "content-Type": "json" });
-            response.write('{"user":"Jean", "role":"admin", "langue":"fr"}');
+            response.write('{"token":"01","user":"Jean", "role":"admin", "langue":"fr"}');
             response.end();
         }
+
         else {
+            console.log("---- 401");
             response.writeHead(401, { "content-type": "json" });
             response.write('{}');
             response.end();
         }
     } else  {
+        console.log("tu n'es pas get");
         response.writeHead(405, { "content-type": "json" });
         response.write('{}');
         response.end();
