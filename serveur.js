aa7f135da7f4859e53633849f50b161887f9cce9
ElodieWDJ <persepolis34@live.fr>
2020-03-03 09:05:35 +0100

    first commut

--- serveur.js
+++ serveur.js
@@ -1,161 +1,49 @@
-let http = require('http');
-let url = require('url');
+var mysql = require('mysql');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-let resultatMathieu = false;
-
-let server = http.createServer(function (request, response) {
-
-
-  let queryObject = url.parse(request.url, true).query;
-
-  if (request.method == "GET") {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    console.log("get");
-
-    
-
-    if (signInUser(queryObject.user, queryObject.password)) {
-      console.log("---- 200")
-      response.writeHead(200, { "content-Type": "json" });
-      response.write('{"token":"01","user":"Sergio", "role":"admin", "langue":"fr"}');
-      response.end();
-    }
-
-    else {
-      console.log("---- 401");
-      response.writeHead(401, { "content-type": "json" });
-      response.write('{}');
-      response.end();
-    }
-  } else {
-    console.log("tu n'es pas get");
-    response.writeHead(405, { "content-type": "json" });
-    response.write('{}');
-    response.end();
-  }
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'nodejs'
 });
 
-//Connexion à la base de données
-const mysql = require('mysql2');
-const express = require('express');
-
-const app = express();
-const port = 3000;
-
-const db = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  database: 'nodejs',
-  user: 'root',
-  password: ''
-})
-
-// sign user method
-function signInUser() {
-
-  db.connect((err) => {
-    if (err){
-      console.log('BORDEL DE MERDE')
-      return (err.message)
-    }
-    else {
-      
-      app.get('/login', async (req, res) => {
-        let result = await connectionDb.promise().query(
-          'SELECT * FROM Users WHERE user = ? AND password = ?',
-          [req.query.user, req.query.password]
-        );
-          console.log(result)
-        if (result[0].length) {
-          console.log(result[0])
-          res.sendStatus(200);
-        } else {
-          console.log('ho putain')
-          res.sendStatus(401);
-        };
-      })
-    };
-  } 
-  )
-
-  return true
-};
-  
- 
+var app = express();
 
 
- //app.listen(port,()=>console.log('Server linstening on port 3000'));
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.component.html'));
+});
 
+app.post('/authentification.service.ts', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
 
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
 
-
-
-// if (user_value == 'jean.jean@jean.com' && password_value == 'jean') {
-//   return true;
-// } else {
-//   return false
-// }
-
-
-// db.connect((err) => {
-//   if (err)
-//     return (err.message)
-//   else {
-//     let  sql  = "SELECT * FROM users WHERE user  = ? AND password = ?"; 
-//     let  inserts  = [ user_value ,  password_value ] ;  
-//     sql  = mysql.format ( sql ,  inserts ) ; 
-//     db.query(sql, (err, result) => {//fonction de callback par rapport à notre requête
-//       if (err)
-//         return false;
-//       else
-//         return true;
-//     }
-
-//     )
-//   };
-// })
-
-// db connexion
-
-// select dans la table user (user et password)
-
-// si ça existe
-// return true
-// sinon
-// rturn false
-
-//   return new Promise(
-//     (resolve, reject) => {
-//       mysql.db.signInWithEmailAndPassword(mail_value, password_value).then(
-//         () => {
-//           resolve();
-//         },
-//         (error) => {
-//           reject(error);
-//         }
-//       );
-//     }
-//   );
-
-
-// db.connect((err) =>{
-//     if (err)
-//         console.log(err.message)
-//     else{
-//         console.log('Connected')
-
-//         db.query('SELECT * FROM users WHERE user_value == mail_value AND password ==password_value',(err, result)=>{//fonction de callback par rapport à notre requête
-//             if (err)
-//                 console.log(err.message)
-//             else
-//                 console.log(result)
-//         })
-
-
-//     }
-// })
-
-
-
-
-server.listen(8080);
+app.listen(3000);
