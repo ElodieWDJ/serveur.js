7e92db187384c3bee3e516a7f279403adac8d752
ElodieWDJ <persepolis34@live.fr>
2020-03-04 11:21:20 +0100

    tout fonctionne

--- serveur.js
+++ serveur.js
@@ -1,110 +1,83 @@
 let http = require('http');
 let url = require('url');
 
-let server = http.createServer(function (request, response) {
+let server = http.createServer(
+	function (request, response) {
+
+		let queryObject = url.parse(request.url, true).query;
+
+		if (request.method == "GET") {
+			response.setHeader('Access-Control-Allow-Origin', '*');
+			console.log("get");
+			console.log(queryObject.user);
+			console.log(queryObject.password);
+			signInUser(queryObject.user, queryObject.password).then(b => gere(response, b, queryObject.user)).catch(err => echec(response))
+
+		} else {
+			console.log("tu n'es pas get");
+			response.writeHead(405, { "content-type": "json" });
+			response.write('{}');
+			response.end();
+		}
+	}
+
+);
+
+function gere(response, b, user) {
+	if (b) {
+		reussite(response, user);
+	} else {
+		echec(response);
+	}
+}
 
 
-  let queryObject = url.parse(request.url, true).query;
+function reussite(response, user) {
+	console.log("---- 200")
+	response.writeHead(200, { "content-Type": "json" });
+	response.write('{"token":"","user":"' + user + '", "role":"", "langue":""}');
+	response.end();
+}
+function echec(response) {
+	console.log("---- 401");
+	response.writeHead(401, { "content-type": "json" });
+	response.write('{}');
+	response.end();
+}
 
-  if (request.method == "GET") {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    console.log("get");
-    if (signInUser(queryObject.user, queryObject.password)) {
-      console.log("---- 200")
-      response.writeHead(200, { "content-Type": "json" });
-      response.write('{"token":"01","user":"Jean", "role":"admin", "langue":"fr"}');
-      response.end();
-    }
-
-    else {
-      console.log("---- 401");
-      response.writeHead(401, { "content-type": "json" });
-      response.write('{}');
-      response.end();
-    }
-  } else {
-    console.log("tu n'es pas get");
-    response.writeHead(405, { "content-type": "json" });
-    response.write('{}');
-    response.end();
-  }
-});
-
-//Connexion à la base de données
-const mysql = require('mysql')
-const db = mysql.createConnection({
-  host: 'localhost',
-  database: 'nodejs',
-  user: 'root',
-  password: ''
-})
 
 // sign user method
-function signInUser(user_value, password_value) {
-  // if (user_value == 'jean.jean@jean.com' && password_value == 'jean') {
-  //   return true;
-  // } else {
-  //   return false
-  // }
-  db.connect((err) => {
-    if (err)
-      return (err.message)
-    else {
-      let  sql  = "SELECT * FROM users WHERE user  = ? AND password = ?"; 
-      let  inserts  = [ user_value ,  password_value ] ;  
-      sql  = mysql.format ( sql ,  inserts ) ; 
-      db.query(sql, (err, result) => {//fonction de callback par rapport à notre requête
-        if (user_value == password_value)
-          return true;
-        else(err)
-          return false;
-      }
+function signInUser(mail_value, password_value) {
 
-      )
-    };
-  })
+
+	//Connexion à la base de données
+	const mysql = require('mysql')
+	const connexion = mysql.createConnection({
+		host: 'localhost',
+		database: 'nodejs',
+		user: 'root',
+		password: ''
+	})
+	let sql = "SELECT * FROM users WHERE mail  = ? AND password = ?";
+	let inserts = [mail_value, password_value];
+	sql = mysql.format(sql, inserts);
+
+	return new Promise((resolve) => {
+		connexion.query(sql, function (error, results, fields) {
+			if (error) throw error;
+			if (results.length > 0) {
+				console.log("FIND!!!!")
+				console.log(results[0]);
+				resolve(true);
+
+			} else {
+				console.log("vide");
+				resolve(false);
+			}
+		});
+	});
+
+
 };
-// db connexion
-
-// select dans la table user (user et password)
-
-// si ça existe
-// return true
-// sinon
-// rturn false
-
-//   return new Promise(
-//     (resolve, reject) => {
-//       mysql.db.signInWithEmailAndPassword(mail_value, password_value).then(
-//         () => {
-//           resolve();
-//         },
-//         (error) => {
-//           reject(error);
-//         }
-//       );
-//     }
-//   );
-
-
-// db.connect((err) =>{
-//     if (err)
-//         console.log(err.message)
-//     else{
-//         console.log('Connected')
-
-//         db.query('SELECT * FROM users WHERE user_value == mail_value AND password ==password_value',(err, result)=>{//fonction de callback par rapport à notre requête
-//             if (err)
-//                 console.log(err.message)
-//             else
-//                 console.log(result)
-//         })
-
-
-//     }
-// })
-
-
-
 
 server.listen(8080);
